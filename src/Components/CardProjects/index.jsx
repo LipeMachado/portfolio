d50d53d1f7206804f056037cd542fd9a128a6c06
LipeMachado <silvalipe765@gmail.com>
2022-04-25 18:02:30 -0300

    Link Fixed

--- src/Components/CardProjects/index.jsx
+++ src/Components/CardProjects/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export function CardProjects(props) {
     return (
-        <a href={props.href}>
+        <a href={props.href} target="_blank">
             <div>
                 <img src={props.src} />
             </div>
