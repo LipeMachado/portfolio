752f8b8f47f406a5c388d2cca76c44a22f6293d8
LipeMachado <silvalipe765@gmail.com>
2022-06-20 17:24:05 -0300

    Add Curriculo

--- src/Sections/Footer/index.jsx
+++ src/Sections/Footer/index.jsx
@@ -11,6 +11,9 @@ import {
 import logoGitHub from '../../Assets/iconsContact/logoGitHub.svg'
 import logoLinkedIn from '../../Assets/iconsContact/logoLinkedIn.svg'
 import logoInstagram from '../../Assets/iconsContact/logoInstagram.svg'
+import logoCurriculo from '../../Assets/iconsContact/logoCurriculo.svg'
+
+import ArchiveCurriculo from '../../Assets/archiveCurriculo/Curriculo-FelipeSilvaMachado.pdf'
 
 export function Footer() {
 
@@ -28,6 +31,7 @@ export function Footer() {
                     <a href="http://github.com/LipeMachado" target="_blank"><img src={logoGitHub} /></a>
                     <a href="https://www.linkedin.com/in/felipe-silva-machado-676b70186/" target="_blank"><img src={logoLinkedIn} /></a>
                     <a href="https://www.instagram.com/lipe_machado765/" target="_blank"><img src={logoInstagram} /></a>
+                    <a href={ArchiveCurriculo} download="Curriculo - Felipe Silva Machado"><img src={logoCurriculo} /></a>
                 </ContactLogosDiv>
                 <ContactIndexsUl>
                     <ContactIndexsDiv>
