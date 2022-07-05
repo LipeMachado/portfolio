33ba58b5b1008291443ee00c49b49416b0e72bfa
LipeMachado <silvalipe765@gmail.com>
2022-07-05 12:31:06 -0300

    Translate

---
+++ src/translate/languages/en.js
@@ -0,0 +1,41 @@
+export const messages = {
+    en: {
+        translations: {
+            navBar: {
+                navBarWhoIAm: 'Who I Am?',
+                navBarProjects: 'Projects',
+                navBarSkills: 'Skills',
+                navBarContact: 'Contact'
+            },
+            home: {
+                homeHello: "Hi, I'm",
+                homeFrontEnd: 'Front-end developer'
+            },
+            whoIAm: {
+                whoIAmTitle: 'Who I Am?',
+                whoIAmParagraph: 'Hello, my name is Felipe Silva Machado. I am a front-end developer, developing responsive and minimalistic websites. I started in the programming world when I was 15, I used to look at games and would like to know how they worked, soon after I started studying python, and was looking for how websites worked, and got interested in developing websites. Today I am a programmer, photographer, video editor and designer.'
+            },
+            projects: {
+                projectsTitle: 'Projects'
+            },
+            skills: {
+                skillsTitle: 'Skills',
+                skillsExample: 'Hover the mouse cursor on the card to read',
+                skillsHtml: 'HTML is a hypertext markup language, which defines the structure of web content.',
+                skillsCss: 'CSS is a style language, used to describe the presentation of a document written in HTML or XML.',
+                skillsJavaScript: 'JavaScript is an interpreted, object-based language with first-class functions, being prototype-based, multi-paradigm and dynamic, supporting object-oriented, imperative and declarative styles.',
+                skillsReactJs: "ReactJs is a library for building user interfaces, React's primary goal is to minimize errors that occur when developers are building UIs(User Interface).",
+                skillsVueJs: 'VueJs is a modern JavaScript framework that provides useful features for progressive enhancement, you can also use Vue to write SPAs(Single Page Applications) applications.',
+                skillsLess: 'Less is a dynamic stylesheet language, the main difference between LESS and other CSS precompilers is that LESS allows real-time compilation by the browser via LESS.js.',
+                skillsStyleComponents: 'Styled Components is a library for React and React Native that allows you to use component-level styles in your application. They are written in a mix of JavaScript and CSS.',
+                skillsPython: 'Python is a high-level programming language, it uses a multi-paradigm approach, which means that it supports procedural, object-oriented, and some functional programming constructs.'
+            },
+            footer: {
+                footerHome: 'Home',
+                footerWhoIAm: 'Who I Am?',
+                footerProjects: 'Projects',
+                footerSkills: 'Skills'
+            }
+        }
+    }
+}
---
+++ src/translate/languages/pt.js
@@ -0,0 +1,41 @@
+export const messages = {
+    pt: {
+        translations: {
+            navBar: {
+                navBarWhoIAm: 'Quem sou eu?',
+                navBarProjects: 'Projetos',
+                navBarSkills: 'Habilidades',
+                navBarContact: 'Contato'
+            },
+            home: {
+                homeHello: 'Olá, Eu sou',
+                homeFrontEnd: 'Desenvolvedor Front-end'
+            },
+            whoIAm: {
+                whoIAmTitle: 'Quem sou eu?',
+                whoIAmParagraph: 'Olá, o meu nome é Felipe Silva Machado. Sou um desenvolvedor front-end, desenvolvo sites responsivos e minimalistas. Comecei no mundo da programação quando tinha 15 anos, costumava ver jogos e gostaria de saber como funcionavam, logo comecei a estudar python, e estava à procura de como funcionavam os sites também, e fiquei interessado em desenvolvimento web. Hoje em dia sou programador, fotógrafo, editor de vídeo e designer.'
+            },
+            projects: {
+                projectsTitle: 'Projetos'
+            },
+            skills: {
+                skillsTitle: 'Habilidades',
+                skillsExample: 'Passe o mouse sobre o cartão para ler',
+                skillsHtml: 'HTML é uma linguagem de marcação de hipertexto, que define a estrutura do conteúdo da web.',
+                skillsCss: 'CSS é uma linguagem de estilo, utilizada para descrever a apresentação de um documento escrito em HTML ou XML.',
+                skillsJavaScript: 'JavaScript é uma linguagem interpretada, baseada em objectos com funções de primeira classe, sendo baseada em protótipos, multi-paradigma e dinâmica, suportando estilos orientados para objectos, imperativos e declarativos',
+                skillsReactJs: 'ReactJs é uma biblioteca para a construção de interfaces de utilizador, o objectivo principal do React é minimizar os erros que ocorrem quando os programadores estão a construir UIs(User Interface).',
+                skillsVueJs: 'O VueJs é uma estrutura JavaScript moderna que fornece características úteis para uma melhoria progressiva, também se pode utilizar o Vue para escrever aplicações SPAs (Single Page Applications).',
+                skillsLess: 'Less é uma linguagem dinâmica de folha de estilo, a principal diferença entre LESS e outros pré-compiladores CSS é que LESS permite a compilação em tempo real pelo navegador através de LESS.js.',
+                skillsStyleComponents: 'Styled Components é uma biblioteca para Reagir e Reagir Native que lhe permite utilizar estilos a nível de componentes na sua aplicação. São escritos numa mistura de JavaScript e CSS.',
+                skillsPython: 'Python é uma linguagem de programação de alto nível, utiliza uma abordagem multi-paradigma, o que significa que suporta construções processuais, orientadas para objectos, e algumas construções de programação funcional.'
+            },
+            footer: {
+                footerHome: 'Início',
+                footerWhoIAm: 'Quem sou eu?',
+                footerProjects: 'Projetos',
+                footerSkills: 'Habilidades'
+            }
+        }
+    }
+}
